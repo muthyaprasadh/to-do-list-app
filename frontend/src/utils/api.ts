acce34867379fc3abab6aab525ecb9d85e6bbb0a
muthya prasadh <muthyakommuru502@gmail.com>
2025-08-04 22:45:55 +0530

    Update api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { User, Task, AuthResponse, TaskFilters } from '@/types';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://to-do-list-app-1-9vwo.onrender.com/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
